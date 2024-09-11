6816d06cf291e231da88d0671b18d98e3f4472a7
AbhinavPamadi <abhinavpamadi@gmail.com>
2024-09-11 19:14:42 +0530

    bins-map

--- public/viewbin.js
+++ public/viewbin.js
@@ -1,5 +1,7 @@
 let locations = [];
 let map; // Declare the map variable here
+let searchInput = document.getElementById("search-input");
+let searchBtn = document.getElementById("search-btn");
 
 function initMap(mapInstance) {
   map = mapInstance; // Assign the map object here
@@ -60,15 +62,35 @@ function removeLocation(lat, lng) {
   }
 }
 
+searchLocation = () => {
+  let location = searchInput.value.trim();
+  if (location) {
+    // Use the Google Maps Geocoder API to get the coordinates of the location
+    let geocoder = new google.maps.Geocoder();
+    geocoder.geocode({ address: location }, (results, status) => {
+      if (status === "OK") {
+        let latLng = results[0].geometry.location;
+        // Pan the map to the new location
+        map.panTo(latLng);
+        // Set the zoom level to 15 to focus on the location
+        map.setZoom(15);
+      } else {
+        alert("Unable to find location");
+      }
+    });
+  }
+};
+
 navigator.geolocation.getCurrentPosition((position) => {
   const { latitude, longitude } = position.coords;
   const mapElement = document.getElementById("map");
   initMap(
     new google.maps.Map(mapElement, {
       center: { lat: latitude, lng: longitude },
-      zoom: 10,
+      zoom: 12,
     })
   );
   mapElement.dataset.lat = latitude;
   mapElement.dataset.lng = longitude;
+  searchBtn.addEventListener("click", searchLocation);
 });
