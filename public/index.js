9b1ec80d40884658ebe21acf886a0bd89b556d66
AbhinavPamadi <abhinavpamadi@gmail.com>
2024-09-15 00:10:51 +0530

    homee

--- public/index.js
+++ public/index.js
@@ -1,61 +1,118 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
-import {getAuth, onAuthStateChanged, signOut} from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
-import{getFirestore, getDoc, doc} from "https://www.gstatic.com/firebasejs/10.13.1/firebase-firestore.js"
+import {
+  getAuth,
+  onAuthStateChanged,
+  signOut,
+} from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
+import {
+  getFirestore,
+  getDoc,
+  doc,
+} from "https://www.gstatic.com/firebasejs/10.13.1/firebase-firestore.js";
+import {
+  getDatabase,
+  set,
+  ref,
+} from "https://www.gstatic.com/firebasejs/10.13.1/firebase-database.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCui4rUSBEeRa0pYFzPBkvFd4amdfCAlM4",
+  authDomain: "reactdemo-84e45.firebaseapp.com",
+  projectId: "reactdemo-84e45",
+  storageBucket: "reactdemo-84e45.appspot.com",
+  messagingSenderId: "921002504696",
+  appId: "1:921002504696:web:886580d928bd8d9357a60d",
+};
 
-    const firebaseConfig = {
-        apiKey: "AIzaSyCui4rUSBEeRa0pYFzPBkvFd4amdfCAlM4",
-        authDomain: "reactdemo-84e45.firebaseapp.com",
-        projectId: "reactdemo-84e45",
-        storageBucket: "reactdemo-84e45.appspot.com",
-        messagingSenderId: "921002504696",
-        appId: "1:921002504696:web:886580d928bd8d9357a60d"
-      };
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
- 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
 
-  const auth=getAuth(app);
-  const db=getFirestore(app);
+onAuthStateChanged(auth, (user) => {
+  const loggedInUserId = localStorage.getItem("loggedInUserId");
+  if (loggedInUserId) {
+    console.log(user);
+    console.log(user.email);
+    const docRef = doc(db, "users", user.uid);
+    console.log(docRef);
+    getDoc(docRef)
+      .then((docSnap) => {
+        if (docSnap.exists()) {
+          const userData = docSnap.data();
+          console.log(userData.firstName);
+        } else {
+          console.log("no document found matching id");
+        }
+      })
+      .catch((error) => {
+        console.log("Error getting document");
+      });
+  } else {
+    console.log("User Id not Found in Local storage");
+  }
+});
 
-  onAuthStateChanged(auth, (user)=>{
-    const loggedInUserId=localStorage.getItem('loggedInUserId');
-    if(loggedInUserId){
-        console.log(user);
-        console.log(user.email)
-        const docRef = doc(db, "users", user.uid);
-        console.log(docRef)
-        getDoc(docRef)
-        .then((docSnap)=>{
-            if(docSnap.exists()){
-                const userData=docSnap.data();
-                console.log(userData.firstName);
-                
+const logoutButton = document.getElementById("logout");
 
-            }
-            else{
-                console.log("no document found matching id")
-            }
-        })
-        .catch((error)=>{
-            console.log("Error getting document");
-        })
-    }
-    else{
-        console.log("User Id not Found in Local storage")
-    }
-  })
-
-  const logoutButton=document.getElementById('logout');
-
-  logoutButton.addEventListener('click',()=>{
-    localStorage.removeItem('loggedInUserId');
-    signOut(auth)
-    .then(()=>{
-        window.location.href='login.html';
+logoutButton.addEventListener("click", () => {
+  localStorage.removeItem("loggedInUserId");
+  signOut(auth)
+    .then(() => {
+      window.location.href = "login.html";
     })
-    .catch((error)=>{
-        console.error('Error Signing out:', error);
-    })
-  })
+    .catch((error) => {
+      console.error("Error Signing out:", error);
+    });
+});
+
+document.getElementById("contact").addEventListener("click", submitForm);
+
+function submitForm(e) {
+  e.preventDefault();
+
+  var name = getElementVal("name");
+  var email = getElementVal("email");
+  var phone = getElementVal("phone");
+  var subject = getElementVal("subject");
+  var msg = getElementVal("msg");
+
+  saveMessages(name, email, phone, subject, msg);
+
+  //   enable alert
+  document.querySelector(".alert").style.display = "block";
+
+  //   remove the alert and rest the form
+  setTimeout(() => {
+    document.querySelector(".alert").style.display = "none";
+    document.getElementById("contactusForm").reset();
+  }, 3000);
+}
+
+const getElementVal = (id) => {
+  return document.getElementById(id).value;
+};
+
+const saveMessages = (name, email, phone, subject, msg) => {
+  const contactusDB = getDatabase(app);
+  set(ref(contactusDB, "Contact/" + name), {
+    name: name,
+    email: email,
+    phone: phone,
+    subject: subject,
+    msg: msg,
+  });
+};
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-10px";
+  }
+}
